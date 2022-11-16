41a735857e5f934d12e3f24578ee940d8a0731b8
InsainArcane <Insainarcane@gmail.com>
2022-11-15 23:43:57 -0500

    11/15/22 Playable Build

---
+++ Project M/Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+
+    public GameObject[] enemies ;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int x = 0;
+
+        while(x < enemies.Length)
+        {
+            enemies[x].SetActive(true);
+            x++;
+        }
+    }
+}
